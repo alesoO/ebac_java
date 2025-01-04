5cb257dccdaeacc1dfaba8b7a795611bcd7db68a
Alexandre <xxalesoaresxx@gmail.com>
2025-01-04 14:09:29 -0300

    Nova Tarefa Concluida

---
+++ src/primary/Calc.java
@@ -0,0 +1,14 @@
+package primary;
+
+public class Calc {
+	
+	public static void main(String[] args) {
+		double n1 = 4.2;
+		double n2 = 7.8;
+		double n3 = 10;
+		double n4 = 3.6;
+		
+		System.out.println("A media é:" + (n1 + n2 + n3 + n4)/4);
+	}
+
+}
