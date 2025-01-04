600afbf09700b96e8e44b7c8557513d1f51d35f5
Alexandre <xxalesoaresxx@gmail.com>
2025-01-04 14:24:48 -0300

    nova tarefa concluida

---
+++ src/primary/Convert.java
@@ -0,0 +1,13 @@
+package primary;
+
+public class Convert {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		Integer convNum = (Integer) num;
+		
+		System.out.println(convNum);
+	}
+
+}
