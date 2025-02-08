bbfa2912b46bb05bbd62c40fcdf44045ec4e7190
Alexandre <xxalesoaresxx@gmail.com>
2025-02-08 14:54:52 -0300

    modulo 29

--- test/br/com/primary/ClienteTest.java
+++ test/br/com/primary/ClienteTest.java
@@ -1,9 +1,33 @@
 package br.com.primary;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ ClienteServiceTest.class, ContratoServiceTest.class })
-public class AllTests {
+import br.com.primary.main.dao.ClienteDao;
+import br.com.primary.main.dao.IClienteDao;
+import br.com.primary.main.domain.Cliente;
+
+public class ClienteTest {
+	
+	@Test
+	public void addTets() throws Exception {
+		IClienteDao dao = new ClienteDao();
+		
+		Cliente cliente = new Cliente();
+		cliente.setCode("01");
+		cliente.setName("Jorge");
+		
+		Integer qtd = dao.add(cliente);
+		assertTrue(qtd == 1);
+		
+		Cliente clienteBD = dao.show(cliente.getCode());
+		assertNotNull(clienteBD);
+		assertNotNull(clienteBD.getId());
+		assertEquals(cliente.getCode(), clienteBD.getCode());
+		assertEquals(cliente.getName(), clienteBD.getName());
+		
+		Integer qtdDel = dao.delete(clienteBD);
+		assertNotNull(qtdDel);
+	}
 }
