54721aeb5747210fdc6d8a1b49238ac4a6729762
Alexandre <xxalesoaresxx@gmail.com>
2025-02-25 20:37:34 -0300

    Modulo 32

--- src/br/com/primary/main/dao/IClienteDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.primary.main.dao;
-
-import br.com.primary.main.dao.generics.IGenericDAO;
-import br.com.primary.main.domain.Cliente;
-
-public interface IClienteDao extends IGenericDAO<Cliente, Long> {
-}
--- src/br/com/primary/main/dao/Persistent.java
+++
@@ -1,6 +0,0 @@
-package br.com.primary.main.dao;
-
-public interface Persistent {
-	public Long getId();
-	public void setId(Long id);
-}
